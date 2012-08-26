c65d4407ce9c925e794cb344e5bbb034aeff6cc3
Thaís <thaysts_1@hotmail.com>
2012-08-26 16:15:06 -0300

    Newsletter

--- pw_olimpiadas/WebContent/files/script.js
+++ pw_olimpiadas/WebContent/files/script.js
@@ -1,3 +1,94 @@
 $(document).ready(function(){
-  $("#featured").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true);
+ 
+  //Slider
+  if ($("#featured").length > 0){
+    $("#featured").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true);
+  }
+
+  //Newsletter
+  if ($("#newsletter-form").length > 0) {
+  
+    jQuery.validator.addMethod("valid_data", function(value, element) { 
+      return isDate(value); 
+    });
+    
+    $("#txtData").mask("99/99/9999");
+
+    $('#newsletter-form').validate({
+      onsubmit: true,
+      onfocusout: false,
+      onkeyup: false,
+      onclick: false,      
+      errorLabelContainer: "#error",
+ 
+      submitHandler: function(form) {
+        $("#error").hide();
+        $("#success").show();
+        $(".required-fields").hide();
+        $("#newsletter-form").hide();
+      },
+      
+      rules: {
+          campoNome: {
+            required: true,
+          },
+          campoData: {
+            required: true,
+            valid_data : true,
+          },
+          campoEmail: {
+            required: true,
+            email: true,
+          },          
+      },
+      
+      messages: {
+        campoNome: {
+          required: "O campo Nome é obrigatório.",
+        },
+        campoData: {
+          required: "O campo Data de Nascimento é obrigatório.",
+          valid_data: "A Data de Nascimento informada não é válida.",
+        },
+        campoEmail: {
+          required: "O campo E-mail é obrigatório.",
+          email: "O E-mail informado não é válido.",
+        },
+      },
+    });    
+  }
+
 });
+
+function isDate(txtDate)
+{
+  var currVal = txtDate;
+  if(currVal == '')
+    return false;
+  
+  //Declare Regex  
+  var rxDatePattern = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/; 
+  var dtArray = currVal.match(rxDatePattern); // is format OK?
+
+  if (dtArray == null)
+     return false;
+ 
+  //Checks for dd/mm/yyyy format.
+  dtMonth = dtArray[3];
+  dtDay= dtArray[1];
+  dtYear = dtArray[5];
+
+  if (dtMonth < 1 || dtMonth > 12)
+      return false;
+  else if (dtDay < 1 || dtDay> 31)
+      return false;
+  else if ((dtMonth==4 || dtMonth==6 || dtMonth==9 || dtMonth==11) && dtDay ==31)
+      return false;
+  else if (dtMonth == 2)
+  {
+     var isleap = (dtYear % 4 == 0 && (dtYear % 100 != 0 || dtYear % 400 == 0));
+     if (dtDay> 29 || (dtDay ==29 && !isleap))
+          return false;
+  }
+  return true;
+}
